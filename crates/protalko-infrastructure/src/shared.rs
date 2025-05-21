7d94a12880bec0ad4af16c1cfe90cf344f1aed2f
reversed_R <qiu1287shan@gmail.com>
2025-05-21 20:35:18 +0900

    feat: default repositories

--- crates/protalko-infrastructure/src/shared.rs
+++ crates/protalko-infrastructure/src/shared.rs
@@ -1,1 +1,24 @@
 pub mod postgresql;
+
+use crate::{shared::postgresql::Postgresql, user::PgUserRepository};
+use protalko_domain::repositories::Repositories;
+
+pub struct DefaultRepositories {
+    pg_user_repository: PgUserRepository,
+}
+
+impl DefaultRepositories {
+    pub fn new(postgresql: Postgresql) -> Self {
+        Self {
+            pg_user_repository: PgUserRepository::new(postgresql),
+        }
+    }
+}
+
+impl Repositories for DefaultRepositories {
+    type UserRepositoryImpl = PgUserRepository;
+
+    fn user_repository(&self) -> &Self::UserRepositoryImpl {
+        &self.pg_user_repository
+    }
+}
