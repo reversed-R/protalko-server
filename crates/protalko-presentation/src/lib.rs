3c6c0076f71129f38932e22ffddaa1f0ccb7a9cc
reversed_R <qiu1287shan@gmail.com>
2025-05-26 18:20:38 +0900

    feat: bin to generate api docs

---
+++ crates/protalko-presentation/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod config;
+pub mod models;
+pub mod modules;
+pub mod routes;
--- crates/protalko-presentation/src/routes.rs
+++ crates/protalko-presentation/src/routes.rs
@@ -6,6 +6,7 @@ use protalko_infrastructure::shared::DefaultRepositories;
 
 use axum::{routing::get, Router};
 use std::sync::Arc;
+use utoipa::OpenApi;
 
 pub fn router(modules: Arc<Modules<DefaultRepositories>>) -> Router {
     Router::new()
@@ -13,3 +14,18 @@ pub fn router(modules: Arc<Modules<DefaultRepositories>>) -> Router {
         .route("/users/{user_id}", get(user::handle_get_by_id))
         .with_state(modules)
 }
+
+use crate::routes;
+#[derive(OpenApi)]
+#[openapi(
+    info(title = "Protalko API"),
+    tags(
+        (name = "health", description = "APIの正常性チェック"),
+        (name = "user", description = "ユーザー関連の操作"),
+    ), 
+    paths(
+        routes::health::handle_health,
+        routes::user::handle_get_by_id
+    )
+)]
+pub struct ApiDocs;
